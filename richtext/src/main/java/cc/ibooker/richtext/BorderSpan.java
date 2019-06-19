3fd4a0589e3bce7b947591b8aa050d5d0348c649
吾非言 <1823132505@qq.com>
2019-06-19 12:27:28 +0800

    init Commit

--- richtext/src/main/java/cc/ibooker/richtext/BorderSpan.java
+++ richtext/src/main/java/cc/ibooker/richtext/BorderSpan.java
@@ -1,7 +1,6 @@
 package cc.ibooker.richtext;
 
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.text.style.ReplacementSpan;
@@ -13,27 +12,34 @@ import android.text.style.ReplacementSpan;
  * <p>
  * https://github.com/zrunker/ZRichText
  */
-public class FrameSpan extends ReplacementSpan {
+public class BorderSpan extends ReplacementSpan {
+    private int start, end;
+    private int color;
     private final Paint mPaint;
     private int mWidth;
 
-    public FrameSpan() {
+    public BorderSpan(int start, int end, int color) {
+        this.start = start;
+        this.end = end;
+        this.color = color;
         mPaint = new Paint();
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setColor(Color.BLUE);
+        mPaint.setColor(color);
         mPaint.setAntiAlias(true);
     }
 
     @Override
     public int getSize(@NonNull Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fm) {
-        //return text with relative to the Paint
+        // return text with relative to the Paint
         mWidth = (int) paint.measureText(text, start, end);
         return mWidth;
     }
 
     @Override
-    public void draw(@NonNull Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, Paint paint) {
-        //draw the frame with custom Paint
+    public void draw(@NonNull Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, @NonNull Paint paint) {
+        if (this.end < start
+                || this.start > end) return;
+        // draw the frame with custom Paint
         canvas.drawRect(x, top, x + mWidth, bottom, mPaint);
         canvas.drawText(text, start, end, x, y, paint);
     }
